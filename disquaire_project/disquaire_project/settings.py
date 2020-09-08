4e62e6a450881bc8ff24119ddbdef77de4b403b4
Modols <thomas.modolo@ynov.com>
2020-09-08 20:20:18 +0200

    Adding data base

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'disquaire_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Disquaire',
+        'NAME': 'disquaire',
         'USER': 'postgres',
         'PASSWORD': 'thomas',
         'HOST': 'localhost',
