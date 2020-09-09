c36cb3ef1fe084285cfdf7d5750b310af3826980
Modols <thomas.modolo@ynov.com>
2020-09-09 18:56:17 +0200

    Adding gabarits and modify the views

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
+
+
 from .models import Album, Artist, Contact, Booking
 
 def index(request):
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    context = {'albums': albums}
+    return render(request, 'store/index.html', context)
 
 def listing(request):
     albums = Album.objects.filter(available=True)
-    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    context = {'albums': albums}
+    return render(request, 'store/listing.html', context)
 
 def detail(request, album_id):
     album = Album.objects.get(pk=album_id)
-    artists = " ".join(artist.name for artist in album.artists.all())
-    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
-    return HttpResponse(message)
+    artists_name = " ".join(artist.name for artist in album.artists.all())
+    context = {
+        'album_title': album.title,
+        'artists_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture
+    }
+    return render(request, 'store/detail.html', context)
 
 def search(request):
     query = request.GET.get('query')
@@ -34,15 +40,10 @@ def search(request):
         if not albums.exists():
             albums = Album.objects.filter(artists__name__icontains=query)
 
-        if not albums.exists():
-            message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-        else:
-            albums = ["<li>{}</li>".format(album.title) for album in albums]
-            message = """
-                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
-                <ul>
-                    {}
-                </ul>
-            """.format("</li><li>".join(albums))
-
-    return HttpResponse(message)
+        
+    title = "Résultat pour la requête %s"%query
+    context = {
+        'albums': albums,
+        'title': title
+    }
+    return render(request, 'store/search.html', context)
