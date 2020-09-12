62d0f46d6b9a7295d36365eb99f1aaecae8eecf0
Modols <thomas.modolo@ynov.com>
2020-09-12 12:06:15 +0200

    Adding Tests

--- disquaire_project/store/tests.py
+++ disquaire_project/store/tests.py
@@ -2,5 +2,53 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
+from .models import Album, Artist, Contact, Booking
 # Create your tests here.
+
+# Test with Index view
+class IndexTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+# Test with Detail view
+class DetailPageTestCase(TestCase):
+    def setUp(self):
+        impossible = Album.objects.create(title="Transmission Impossible")
+        self.album = Album.objects.get(title='Transmission Impossible')
+
+    def test_detail_page_returns_200(self):
+        album_id = self.album.id
+        response = self.client.get(reverse('store:detail', args=(album_id,)))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_detail_page_returns_404(self):
+        album_id = self.album.id + 1
+        response = self.client.get(reverse('store:detail', args=(album_id,)))
+        self.assertEqual(response.status_code, 404) 
+
+# Test with Booking view
+
+class BookingPageTestCase(TestCase):
+    def setUp(self):
+        Contact.objects.create(name="Freddie", email="fred@queens.forever")
+        impossible = Album.objects.create(title="Transmission Impossible")
+        journey = Artist.objects.create(name="Journey")
+        impossible.artists.add(journey)
+        self.contact = Contact.objects.get(name='Freddie')
+        self.album = Album.objects.get(title='Transmission Impossible')
+
+    def test_new_booking_is_registered(self):
+        old_bookings = Booking.objects.count()
+        album_id = self.album.id
+        name = self.contact.name
+        email = self.contact.email
+        response = self.client.post(reverse('store:detail', args=(album_id,)), {
+            'name': name,
+            'email': email
+        })
+        new_bookings = Booking.objects.count()
+        self.assertEqual(new_bookings, old_bookings + 1)
+        
