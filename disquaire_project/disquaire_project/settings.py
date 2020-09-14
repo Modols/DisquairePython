76ca06ac921f2d2a6a3ef209a5df299df726d163
Modols <67863922+Modols@users.noreply.github.com>
2020-09-14 10:17:58 +0200

    Update settings.py

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8dt^q2gl%!dg8r7z_%(c&-oy#$0jcp3&9(s%a)dd*q(!cbpwwr'
+SECRET_KEY = 'Your Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
